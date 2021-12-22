5bdaf2b1c4e524dc7df92bd7ad29fc1222660bc1
Lindsey Beckle <justbecks16@gmail.com>
2021-12-22 15:09:06 -0800

    Setting up card grid

--- src/App.js
+++ src/App.js
@@ -2,36 +2,30 @@ import { useState } from 'react'
 import './App.css'
 
 
-//arrays can be created outside the component because they are a constant and will never need to change
-//this also means that the array will not need to be recreated every time the component is re-evaluated
-
 //CARD ARRAY
+//each is an object with 1 prop 
 const cardImages = [
   { "src": "/img/helmet-1.png" },
-  { "src": "/img/portion-1.png" },
+  { "src": "/img/potion-1.png" },
   { "src": "/img/ring-1.png" },
   { "src": "/img/scroll-1.png" },
   { "src": "/img/shield-1.png" },
   { "src": "/img/sword-1.png" },
-  //each one is an object with 1 property, which is a src property 
+
 ]
 
-//function inside component 
+//shuffle card function 
 function App() {
-  //function will have 3 tasks 
     const [cards, setCards] = useState([])
     const [turns, setTurns] = useState(0)
+
   //shuffle cards 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages] 
-    //spread syntax takes each of the cardImages and dulicates it 
     //creates an array of 12 cards (6 each)
     .sort(() => Math.random() - 0.5)
     //fires a function for each item or pair of items in the above array [...cardImages, ...cardImages]
-    //if we return a number less than 0, the order of items stays the same
-    //greater than 0, order of 2 items gets mixed up
     .map((card) => ({...card, id: Math.random() }))
-    //fires a function for each item in the sorted array; for each one, it adds an id property and will return an object
     // ...card: takes the card and it's properties (currently only prop is src )
     //id: Match.random() sets a random ID on each object
 
@@ -48,6 +42,17 @@ function App() {
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <div className="card" key={card.id}>
+            <div>
+              <img className="front" src={card.src} alt="card front" />
+              <img className="back" src="/img/cover.png" alt="card back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
