ca9f89e9220012db0c996a1ae7ae589da9c4ef0c
Lindsey Beckle <justbecks16@gmail.com>
2021-12-22 10:52:43 -0800

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    //app div
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button>New Game</button>
+    </div>
+  );
+}
+
+export default App
